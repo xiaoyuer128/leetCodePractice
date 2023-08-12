def0d3edc64540c74f81b03e61e912babcfcf527
xiaoyuer <1119756165@qq.com>
2023-08-12 17:58:19 +0800

    20230812
    日常提交

--- src/practice03.java
+++ src/practice03.java
@@ -8,7 +8,7 @@ public class practice03 {
         int m = romanToInt(sc.next());
         System.out.println(m);
     }
-    public static int romanToInt(String s) {
+   /* public static int romanToInt(String s) {
         int sum= 0;
         if(s.equals("IV"))return 4;
         else if (s.equals("IX")) return 9;
@@ -90,4 +90,36 @@ public class practice03 {
 
 
     }
-}
+}*/
+
+
+        public static int romanToInt(String s) {
+            int sum = 0;
+            int preNum = getValue(s.charAt(0));
+            for(int i = 1;i < s.length(); i ++) {
+                int num = getValue(s.charAt(i));
+                if(preNum < num) {
+                    sum -= preNum;
+                } else {
+                    sum += preNum;
+                }
+                preNum = num;
+            }
+            sum += preNum;
+            return sum;
+        }
+
+        private static int getValue(char ch) {
+            switch(ch) {
+                case 'I': return 1;
+                case 'V': return 5;
+                case 'X': return 10;
+                case 'L': return 50;
+                case 'C': return 100;
+                case 'D': return 500;
+                case 'M': return 1000;
+                default: return 0;
+            }
+        }
+    }
+
