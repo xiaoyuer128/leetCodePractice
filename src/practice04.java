f76b99ce2bb00fb51cd4b7b1c696720139aac79b
xiaoyuer <1119756165@qq.com>
2023-08-17 19:13:39 +0800

    2023/08/17

---
+++ src/practice04.java
@@ -0,0 +1,11 @@
+public class practice04 {
+    public static void main(String[] args) {
+        System.out.println("请输入字符数组");
+
+    }
+    public String longestCommonPrefix(String[] strs) {
+
+
+
+    }
+}
